de3f7f8b5466a4b085023a0bfc6f13c535ba8316
SebastianG03 <guamandavid11235@gmail.com>
2024-01-09 21:37:02 -0500

    Pagina arreglada

---
+++ EjemploModelado.Web/Domain/ServiceModel/ComputadoraModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EjemploModelado.Web.Domain.ServiceModel
+{
+    public class ComputadoraModel
+    {
+        public int MemoriaRam { get; set; }
+        public int Almacenamiento { get; set; }
+        public int NucleosProcesador { get; set; }
+        public int PuertosUsb { get; set; }
+        public IEnumerable<int> ComponentesIds { get; set; } = new List<int>();
+    }
+}
